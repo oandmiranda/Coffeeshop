43101a37521955f9dc348abf98069244062c1428
Anderson Miranda <and.miranda1818@gmail.com>
2023-09-02 11:24:19 -0300

    corrigido bug

--- src/Components/Header/styles.ts
+++ src/Components/Header/styles.ts
@@ -1,7 +1,7 @@
 import { styled } from "styled-components"
 
 export const Container = styled.header`
-    width: 100%;
+    width: 100vw;
     height: 100vh;
     display: flex;
     flex-direction: column;
