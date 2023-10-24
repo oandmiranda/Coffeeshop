d284d882ef08293e555f73ff1566ff279abbfbb9
Anderson Miranda <and.miranda1818@gmail.com>
2023-10-23 21:58:27 -0300

    added new route

---
+++ src/routes.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./Styles/themes";
+import GlobalStyle from "./Styles/globalStyle";
+import Home from "./pages/Home";
+import Category from "./pages/Category";
+import MyHistory from "./pages/MyHistory";
+
+const AppRoutes = () => {
+    return (
+        <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={ <Home /> }></Route>
+                    <Route path="/categoria/:categoryRoute" element={ <Category /> }></Route>
+                    <Route path="/myhistory" element={ <MyHistory /> }></Route>
+                </Routes>
+            </BrowserRouter>
+        </ThemeProvider>
+    )
+}
+
+export default AppRoutes;
