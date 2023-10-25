408cc1938fcab1c6ae694f9ac4189cc5f62d1cf9
Anderson Miranda <and.miranda1818@gmail.com>
2023-10-24 21:56:07 -0300

    adicionado seção de história e corroussel

--- src/pages/MyHistory/index.tsx
+++ src/pages/MyHistory/index.tsx
@@ -1,9 +1,12 @@
 import Content from "../../Components/Content";
+import Footer from "../../Components/Footer";
 import Header from "../../Components/Header";
 import coverImage from "../../assets/img/coffeeshop.png";
+import coverImage2 from "../../assets/img/cafeteria.png";
 
 const headerHome = [
-    { id: 1, coverImage: coverImage, name: 'coffeeshop', text: "Desde 1979 servindo o melhor café de São Paulo", isVisible: false }
+    { id: 1, coverImage: coverImage2, name: 'coffeeshop', text: "", isVisible: false },
+    { id: 2, coverImage: coverImage, name: 'coffeeshop', text: "", isVisible: false },
   ]
 
 const MyHistory = () => {
@@ -25,12 +28,16 @@ const MyHistory = () => {
             <Header {...props} />
             <Content 
                 reverse
+                btnVisible={false}
                 backgroundImage={coverImage}
-                title="Nossa História"
-                h4_children="hello"
+                positionCenter={false}
+                title="Nosso grão"
                 background_Button="gray"
-                children={"hello word"}
+                paragraph="Nossa jornada em busca da melhor xícara de café nos levou a uma das mais tradicionais cafeterias de São Paulo, o [nome da cafeteria]. Localizado no coração da cidade, este estabelecimento representa a fusão perfeita entre a tradição cafeeira e o paladar moderno.
+                Fundado há mais de meio século por um apaixonado por café, o [nome da cafeteria] tem uma história profundamente enraizada na cultura do café.
+                Os grãos escolhidos a dedo são submetidos a um processo de torra que é uma verdadeira obra de arte. Os torrefadores experientes da cafeteria dominam a ciência e a arte de revelar os sabores intrínsecos de cada variedade de café. "
             />
+            <Footer />
         </>
     )
 }
