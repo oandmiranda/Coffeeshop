a2411a8b9516732e1bc369323d88bb2ce4a2a477
Anderson Miranda <and.miranda1818@gmail.com>
2023-11-03 06:55:02 -0300

    hook useMemo adicionado a page Category

--- src/pages/Category/index.jsx
+++ src/pages/Category/index.jsx
@@ -1,31 +1,34 @@
 import { useSelector } from "react-redux";
 import { Link, useParams } from 'react-router-dom';
+import { useMemo } from "react";
 import Container from "../../Components/Container";
 import Header from "../../Components/Header";
 import { Image, ItemContainer, Description, ButtonCard } from "../../Components/Container/styles";
 import ItemCategory from "../../Components/ItemCategory";
 
 const Category = () => {
+
     const { categoryRoute } = useParams();
-    const { categories, items } = useSelector(state => ({
-      categories: state.categories.filter(category => category.id === categoryRoute),
-      items: state.items.filter(item => item.category === categoryRoute),
-    }));
-   
+    const state = useSelector(state => state);
+    const categories = useMemo(() => state.categories.filter(category => category.id === categoryRoute), [state.categories, categoryRoute]);
+    const items = useMemo(() => state.items.filter(item => item.category === categoryRoute), [state.items, categoryRoute]);
+    
     const images = useSelector(state => state.items);
-    const filteredImages = images.reduce((acc, image) => {
-      if (!acc[image.category]) {
-        acc[image.category] = [];
-      }
-      acc[image.category].push(image);
-      return acc;
-    }, {});
-   
+    const filteredImages = useMemo(() => {
+      return images.reduce((acc, image) => {
+        if (!acc[image.category]) {
+          acc[image.category] = [];
+        }
+        acc[image.category].push(image);
+        return acc;
+      }, {});
+    }, [images]);
+ 
     const currentCategoryImages = filteredImages[categoryRoute];
     
-    const props = {
+    const props = useMemo(() => ({
         content: currentCategoryImages,
-        height: '75vh',
+        height: '85vh',
         children: 'Ver lista',
         subtitle: categories.name,
         positionTop: "45%",
@@ -33,7 +36,8 @@ const Category = () => {
         positionRight: "0",
         color: "#fff",
         fontSize: "1.3rem",
-    }
+    }), [currentCategoryImages, categories.name]);
+
 
     return (
         <>
