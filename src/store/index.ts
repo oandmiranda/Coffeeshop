4e5f69a4e78cd5f46ba402f755d9d44f68dc673a
Anderson Miranda <and.miranda1818@gmail.com>
2023-11-12 12:34:03 -0300

    novo slice coffeeshopAreas adicionado

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,20 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import categoriesSlice from './reducer/categories';
 import itemsSlice from './reducer/items'; 
 import headerContentSlice from './reducer/header';
-// import { TypedUseSelectorHook } from 'react-redux';
-// import { useSelector } from 'react-redux';
-
-
-// export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+import coffeeshopAreaSlice from "./reducer/coffeeshopAreas";
 
 const store = configureStore({
     reducer: {
         categories: categoriesSlice,
         items: itemsSlice,
         headerContent: headerContentSlice,
+        coffeeshopAreas: coffeeshopAreaSlice
     }
-})
-
-// export type RootState = ReturnType<typeof store.getState>
+});
 
 export default store;
