acbf578532e74dd8e8bab4e9f9350968b8673a30
Anderson Miranda <and.miranda1818@gmail.com>
2023-11-27 20:31:49 -0300

    componente refatorado

--- src/Components/Title/index.tsx
+++ src/Components/Title/index.tsx
@@ -1,11 +1,12 @@
 import { TitleProps } from "../../Types/userTypes";
 import { StyledTitle } from "./styles";
 
-const Title = ({fontSize, children, color}: TitleProps) => {
+const Title = ({fontSize, children, color, style}: TitleProps) => {
     return (
         <StyledTitle
             fontSize={fontSize}
             color={color}
+            style={style}
         >
             {children}
         </StyledTitle>
