b147a5a0155558656f30bdc661f768c3165e4296
Anderson Miranda <and.miranda1818@gmail.com>
2023-12-11 21:09:36 -0300

    adicionado componente Burger Menu responsivo

---
+++ src/Components/BurgerMenu/index.tsx
@@ -0,0 +1,22 @@
+import { slide as Menu } from 'react-burger-menu';
+import { Link } from 'react-router-dom';
+import "./styles.css";
+
+interface BurgerMenuProps {
+    pageWrapId: string;
+    outerContainerId: string;
+}
+
+const BurgerMenu = ({ pageWrapId, outerContainerId }: BurgerMenuProps) => {
+    return (
+      <Menu pageWrapId={"page-wrap"} outerContainerId={"outer-container"}>
+        <Link to="/" className="menu-item">Home</Link>
+        <Link to="/myhistory" className="menu-item">Nossa História</Link>
+        <Link to="/" className="menu-item">Lojas</Link>
+        <Link to="/franchise" className="menu-item">Seja um franqueado</Link>
+        <Link to="/" className="menu-item">Aprenda mais</Link>
+      </Menu>
+    );
+  };
+
+export default BurgerMenu;
