ece07092def1a24d63584e24cdcb65a5853b8721
Anderson Miranda <and.miranda1818@gmail.com>
2023-12-18 13:10:47 -0300

    adicionado responsividade mobileS

--- src/Components/ItemCategory/styles.ts
+++ src/Components/ItemCategory/styles.ts
@@ -1,4 +1,5 @@
 import { styled } from "styled-components";
+import { device } from "../../Styles/themes";
 
 export const Section = styled.div`
     margin: 40px auto;
@@ -10,25 +11,47 @@ export const Section = styled.div`
     justify-content: space-around;
     padding-block: 20px;
     position: relative;
+
+    @media ${device.mobileS} {
+        flex-wrap: wrap;
+        gap: 25px;
+        padding: 25px 20px;
+    }
 `;
 
 export const Item = styled.li`
-    padding-top: 60px;
+    padding-top: 25px;
     display: flex;
     align-items: center;
+
+    @media ${device.mobileS} {
+            margin: -35px 1px;
+        };
 
     div {
         display: flex;
         flex-direction: column;
         align-items: center;
         gap: 10px;
-    }
+        margin-top: 30px;
+
+        @media ${device.mobileS} {
+            margin-top: 54px;
+            margin-bottom: 30px;
+            gap: 2px;
+        };
+    };
 `;
 
 export const Image = styled.img`
     width: 140px;
     height: 140px;
     border-radius: 50%;
+
+    @media ${device.mobileS} {
+        width: 120px;
+        height: 120px;
+    };
 `;
 
 export const Description = styled.h3`
