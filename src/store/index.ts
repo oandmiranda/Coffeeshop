f81aeb98f17d984fa1105367a2237e37e8336515
Anderson Miranda <and.miranda1818@gmail.com>
2023-12-18 13:02:23 -0300

    novo slice Stores adicionado

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,13 +3,15 @@ import categoriesSlice from './reducer/categories';
 import itemsSlice from './reducer/items'; 
 import headerHomeSlice from './reducer/headerHome';
 import coffeeshopAreaSlice from "./reducer/coffeeshopAreas";
+import headerStoresSlice from './reducer/headerStores';
 
 const store = configureStore({
     reducer: {
         categories: categoriesSlice,
         items: itemsSlice,
         headerHome: headerHomeSlice,
-        coffeeshopAreas: coffeeshopAreaSlice
+        coffeeshopAreas: coffeeshopAreaSlice,
+        headerStores: headerStoresSlice
     }
 });
 
